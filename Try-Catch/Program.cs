5d73c0ad65ba68b61de24ed5951b0691b6ef6789
Silas Augusto da Cunha <0997s.a.cunha@gmail.com>
2024-11-13 22:25:48 +0000

    try-catch teste de erro de execucao

---
+++ Try-Catch/Program.cs
@@ -0,0 +1,12 @@
+﻿try{
+double float1 = 3000.0;
+double float2 = 0.0;
+int number1 = 3000;
+int number2 = 0;
+
+Console.WriteLine(float1 / float2);
+Console.WriteLine(number1 / number2);
+Console.WriteLine("Exit program");
+} catch{
+    Console.WriteLine($"Erro na execução");
+}
